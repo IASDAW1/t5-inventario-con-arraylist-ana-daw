430512fe5a041167cca78e086a50ef0d3629c4b1
Ana Belen <Ana Belen@DESKTOP-5JL2VR6>
2024-01-12 12:34:37 +0100

    prueba

---
+++ inventarioGH/src/main/java/com/miproyecto/inventariogh/InventarioGH.java
@@ -0,0 +1,13 @@
+
+package com.miproyecto.inventariogh;
+
+/**
+ *
+ * @author Ana Belen
+ */
+public class InventarioGH {
+
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+}
